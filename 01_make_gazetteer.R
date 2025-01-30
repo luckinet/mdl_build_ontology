8510e6a2a07bc0e65d4b44d94cec4b2c409bc996
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2025-01-30 18:18:34 +0100

    revert back to version 3.6

--- 01_make_gazetteer.R
+++ 01_make_gazetteer.R
@@ -3,7 +3,7 @@
 # description : This is a script for building the LUCKINet territory gazetteer.
 # license     : https://creativecommons.org/licenses/by-sa/4.0/
 # authors     : Steffen Ehrmann
-# date        : 2024-01-09
+# date        : 2024-09-27
 # version     : 1.0.0
 # status      : done
 # comment     : file.edit(paste0(dir_docs, "/documentation/mdl_build_ontology.md"))
@@ -13,23 +13,21 @@ message("\n---- build gazetteer for territories ----")
 
 # make paths ----
 #
-path_gadm <- paste0(.get_path("onto", "_data"), "gadm_410-levels.gpkg")
-path_gadm_zip <- paste0(.get_path("onto", "_data"), "gadm_410-levels.zip")
-path_geoschemeToGADM <- paste0(.get_path("onto", "_data"), "geoscheme_to_gadm.rds")
+path_gadm <- paste0(.get_path("onto", "_data"), "gadm36_levels.gpkg") # paste0(dir_data, "gadm_410-levels.gpkg")
 
 # load data ----
 #
 # unpack the file, if it's not yet unpacked
 if(!testFileExists(path_gadm)){
-  if(!testFileExists(path_gadm_zip)){
-    stop("please store the GADM dataset in '", .get_path("onto", "_data"), "'")
+  if(!testFileExists(paste0(.get_path("onto", "_data"), "gadm36_levels_gpkg.zip"))){
+    stop("please store 'gadm36_levels_gpkg.zip' in '", .get_path("onto", "_data"), "'")
   } else {
     message(" --> unpacking GADM basis")
-    unzip(path_gadm_zip, exdir = .get_path("onto", "_data"))
+    unzip(paste0(.get_path("onto", "_data"), "gadm36_levels_gpkg.zip"), exdir = .get_path("onto", "_data"))
   }
 }
 
-gadm <- st_read(dsn = path_gadm, layer = "ADM_0") |>
+gadm <- st_read(dsn = path_gadm, layer = "level0") |>
   st_drop_geometry()
 gadm_layers <- st_layers(dsn = path_gadm)
 
@@ -39,11 +37,11 @@ geoscheme <- read_csv2(file = paste0(.get_path("onto", "_data"), "UNSD — Metho
 #
 other <- tibble(un_region = c("Asia", "Europe", "Asia"),
                 un_subregion = c("Eastern Asia", "Southern Europe", "Western Asia"),
-                iso_a3 = c("TWN", "XKO", "ZNC"),
+                iso_a3 = c("TWN", "XKO", "XNC"),
                 unit = c("Taiwan", "Kosovo", "Northern Cyprus"))
 
 # first, build the UN geoscheme ...
-temp_geoseries <- geoscheme |>
+temp_geo <- geoscheme |>
   mutate(un_subregion = if_else(!is.na(`Intermediate Region Name`), `Intermediate Region Name`, `Sub-region Name`)) |>
   select(unit = `Country or Area`,
          un_region = `Region Name`,
@@ -52,49 +50,11 @@ temp_geoseries <- geoscheme |>
          iso_a2 = `ISO-alpha2 Code`,
          iso_a3 = `ISO-alpha3 Code`) |>
   filter(!is.na(un_region)) |>  # this filters out only Antarctica
-  bind_rows(other)
+  bind_rows(other) |>
+  full_join(gadm, by = c("iso_a3" = "GID_0")) |>
+  filter(!is.na(un_region))
 
-message(" --> clean and reshape GADM")
-for(i in 6:1){
-
-  message("     ADM ", i-1)
-
-  new <- st_read(dsn = path_gadm, layer = gadm_layers$name[i]) |>
-    st_drop_geometry() |>
-    as_tibble() |>
-    # remove empty spaces and dots
-    mutate(across(all_of(contains("NAME_")),
-                  function(x){
-                    temp <- trimws(x)
-                    str_replace_all(string = temp, pattern = "[.]", replacement = "")
-                  })) |>
-    # remove water bodies
-    filter(if_any(ends_with(paste0("ENGTYPE_", i-1)), ~ !str_detect(string = .x, pattern = "Water body|Water Body|Waterbody"))) |>
-    filter(if_all(ends_with(paste0("TYPE_", i-1)), ~ !str_detect(string = .x, pattern = "Water body|Water Body|Waterbody"))) |>
-    # make NAs explicit
-    mutate(across(everything(), ~na_if(x = ., y = "NA")))
-
-  if(i == 6){
-    gadm_full <- new
-  } else {
-    new <- new |>
-      filter(!(!!sym(paste0("GID_", i-1)) %in% unique(gadm_full[[paste0("GID_", i-1)]])))
-    gadm_full <- gadm_full |>
-      bind_rows(new)
-  }
-
-}
-
-gadm_full <- gadm_full |>
-  rename(NAME_0 = COUNTRY) |>
-  select(GID_0, NAME_0, ends_with("_1"), ends_with("_2"), ends_with("_3"), ends_with("_4"), ends_with("_5")) |>
-  arrange(NAME_0, NAME_1, NAME_2, NAME_3, NAME_4, NAME_5) |>
-  filter(!is.na(GID_0)) |>
-  full_join(temp_geoseries, by = c("GID_0" = "iso_a3")) |>
-  filter(!is.na(unit)) |>
-  mutate(NAME_0 = unit)
-
-# start a new ontology
+# ... then, start a new ontology
 message(" --> initiate gazetteer")
 gazetteer <- start_ontology(name = "lucki_gazetteer", path = .get_path("onto", "_data"),
                             version = "1.0.0",
@@ -102,21 +62,13 @@ gazetteer <- start_ontology(name = "lucki_gazetteer", path = .get_path("onto", "
                             description = "the intial LUCKINet gazetteer",
                             homepage = "https://www.luckinet.org",
                             license = "https://creativecommons.org/licenses/by/4.0/",
-                            notes = "This gazetteer nests each country and their sub-level territories into the United Nations geoscheme.")
+                            notes = "This gazetteer nests each country and their sub-level territories into the United Nations geoscheme. In parallel, as some concepts span several countries, for example 'France', and as these concepts would then not be nestable into the UN geoscheme, there is in parallel also a class 'nation' (at the first level) that is for those concepts that might occurr in the thematic described with the ontology. ")
 
 # define GADM as source
 gazetteer <- new_source(name = "gadm",
-                        version = "4.1",
+                        version = "3.6",
                         date = Sys.Date(),
-                        description = "GADM wants to map the administrative areas of all countries, at all levels of sub-division. We provide data at high spatial resolutions that includes an extensive set of attributes.",
-                        homepage = "https://gadm.org/index.html",
-                        license = "https://creativecommons.org/licenses/by/4.0/",
-                        ontology = gazetteer)
-
-gazetteer <- new_source(name = "alt",
-                        version = "4.1",
-                        date = Sys.Date(),
-                        description = "alternative territory names according to GADM",
+                        description = "GADM wants to map the administrative areas of all countries, at all levels of sub-division. We provide data at high spatial resolutions that includes an extensive set of attributes. ",
                         homepage = "https://gadm.org/index.html",
                         license = "https://creativecommons.org/licenses/by/4.0/",
                         ontology = gazetteer)
@@ -126,28 +78,28 @@ gazetteer <- new_class(new = "un_region", target = NA,
                        description = "region according to the UN geoscheme", ontology = gazetteer)
 gazetteer <- new_class(new = "un_subregion", target = "un_region",
                        description = "sub-region according to the UN geoscheme", ontology = gazetteer)
-
-for(i in 0:model_info$parameters$ADM_max){
-
-  if(i == 0){
-    gazetteer <- new_class(new = paste0("ADM", i), target = "un_subregion",
-                           description = "the first administrative level of the GADM gazetteer", ontology = gazetteer)
-  } else {
-    gazetteer <- new_class(new = paste0("ADM", i), target = paste0("ADM", i-1),
-                           description = "the first administrative level of the GADM gazetteer", ontology = gazetteer)
-  }
-
-}
+gazetteer <- new_class(new = "ADM0", target = "un_subregion",
+                       description = "the first administrative level of the GADM gazetteer", ontology = gazetteer)
+gazetteer <- new_class(new = "ADM1", target = "ADM0",
+                       description = "the second administrative level of the GADM gazetteer", ontology = gazetteer)
+gazetteer <- new_class(new = "ADM2", target = "ADM1",
+                       description = "the third administrative level of the GADM gazetteer", ontology = gazetteer)
+gazetteer <- new_class(new = "ADM3", target = "ADM2",
+                       description = "the fourth administrative level of the GADM gazetteer", ontology = gazetteer)
+gazetteer <- new_class(new = "ADM4", target = "ADM3",
+                       description = "the fifth administrative level of the GADM gazetteer", ontology = gazetteer)
+gazetteer <- new_class(new = "ADM5", target = "ADM4",
+                       description = "the sixth administrative level of the GADM gazetteer", ontology = gazetteer)
 
 # define the harmonised concepts
 message("     UN geoscheme")
-un_region <- toupper(unique(temp_geoseries$un_region))
+un_region <- toupper(unique(temp_geo$un_region))
 
 gazetteer <- new_concept(new = un_region,
                          class = "un_region",
                          ontology =  gazetteer)
 
-un_subregion <- temp_geoseries |>
+un_subregion <- temp_geo |>
   select(broader = un_region, concept = un_subregion) |>
   distinct() |>
   mutate(broader = toupper(broader))
@@ -159,100 +111,96 @@ gazetteer <- new_concept(new = tempConcepts$concept,
                          class = "un_subregion",
                          ontology =  gazetteer)
 
+for(i in 1:6){
 
-message(" --> filling gazetteer")
-for(i in 0:model_info$parameters$ADM_max){
+  message("     GADM level ", i)
 
-  if(i == 0){
-    thisLabel <- "NAME_0"
-
-    temp <- gadm_full |>
-      select(ends_with(paste0("_", 0:i)), "un_subregion") |>
-      mutate(alt = NA) |>
-      rename(previous_label = "un_subregion") |>
-      distinct(!!sym(thisLabel), .keep_all = TRUE)
-
-    previous <- get_concept(label = temp$previous_label, ontology = gazetteer) |>
-      mutate(previous_label = label) |>
-      select(-ends_with("_match"))
+  thisLabel <- paste0("NAME_", i-1)
+  if(i == 1){
+    prevLabel <- "un_subregion"
   } else {
-    prevLabel <- thisLabel
-    thisLabel <- paste0("NAME_", i)
+    prevLabel <- paste0("NAME_", i-2)
+  }
 
-    temp <- gadm_full |>
-      select(ends_with(paste0("_", 0:i)))
-    if(any(colnames(gadm_full) %in% c(paste0("VARNAME_", i), paste0("NL_NAME_", i)))){
-      temp <- temp |>
-        unite(col = "alt", any_of(paste0("VARNAME_", i)), any_of(paste0("NL_NAME_", i)), sep = "|", na.rm = TRUE) |>
-        mutate(across(alt, ~na_if(x = ., y = "")))
-    } else {
-      temp <- temp |>
-        mutate(alt = NA)
-    }
-    temp <- temp |>
-      unite(col = "previous_label", sort(str_subset(colnames(temp), "^NAME_"))[1:i], sep = ".", na.rm = TRUE, remove = FALSE) |>
-      group_by(!!!syms(paste0("NAME_", 0:(i-1)))) |>
-      distinct(!!sym(thisLabel), .keep_all = TRUE) |>
-      ungroup() |>
-      filter(!is.na(!!sym(paste0("GID_", i))))
+  temp <- st_read(dsn = path_gadm, layer = gadm_layers$name[i]) |>
+    st_drop_geometry() |>
+    filter(if_any(matches(paste0("ENGTYPE_", i-1)), ~ !.x %in% c("Water body", "Water Body", "Waterbody"))) |>
+    as_tibble() |>
+    select(starts_with("NAME_")) |>
+    mutate(across(all_of(contains("NAME_")),
+                  function(x){
+                    temp <- trimws(x)
+                    str_replace_all(string = temp, pattern = "[.]", replacement = "")
+                  }))
 
-    if(i == 1) {
-      previous <- get_concept(label = items[[prevLabel]], has_broader = items$id, class = paste0("ADM", i-1), ontology = gazetteer) |>
-        mutate(previous_label = label)|>
-        select(-ends_with("_match"))
-    } else {
-      previous <- get_concept(label = items[[prevLabel]], has_broader = items$id, class = paste0("ADM", i-1), ontology = gazetteer) |>
-        left_join(previous |> select(id, previous_label) |> distinct(), c("has_broader" = "id")) |>
-        unite(col = "previous_label", previous_label, label, sep = ".", na.rm = TRUE, remove = FALSE)|>
-        select(-ends_with("_match"))
-   }
+  mapGADM <- temp |>
+    full_join(temp_geo, by = "NAME_0") |>
+    mutate(!!thisLabel := if_else(is.na(!!sym(thisLabel)), !!sym(prevLabel), !!sym(thisLabel))) |>
+    select(-NAME_0) |>
+    rename(NAME_0 = unit) |>
+    filter(!is.na(un_subregion))
+
+  if(i == 1){
+
+    mapGADM <- mapGADM |>
+      rename("previous_label" = !!prevLabel)
+
+    previous <- get_concept(label = un_subregion$concept, ontology = gazetteer) |>
+      rename(previous_label = label)
+
+  } else if(i == 2) {
+    mapGADM <- mapGADM |>
+      unite(col = "previous_label", sort(str_subset(colnames(temp), "^NAME_"))[1:(i-1)], sep = ".", na.rm = TRUE, remove = FALSE)
+    previous <- get_concept(label = items[[prevLabel]], has_broader = items$id, class = paste0("ADM", i-2), ontology = gazetteer) |>
+      rename(previous_label = label)
+  } else {
+
+    mapGADM <- mapGADM |>
+      unite(col = "previous_label", sort(str_subset(colnames(temp), "^NAME_"))[1:(i-1)], sep = ".", na.rm = TRUE, remove = FALSE)
+
+    previous <- get_concept(label = items[[prevLabel]], has_broader = items$id, class = paste0("ADM", i-2), ontology = gazetteer) |>
+      left_join(previous |> select(id, previous_label), c("has_broader" = "id")) |>
+      unite(col = "previous_label", previous_label, label, sep = ".", na.rm = TRUE, remove = FALSE)
 
   }
 
-  items <- temp |>
-    mutate(!!thisLabel := if_else(is.na(!!sym(thisLabel)), !!sym(paste0("GID_", i)), !!sym(thisLabel))) |>
+  items <- mapGADM |>
     left_join(previous, by = "previous_label") |>
-    filter(!is.na(id))
+    distinct() |>
+    filter(!is.na(!!sym(thisLabel)) & !is.na(id))
 
-  # assign the new concepts into the ontology
-  gazetteer <- new_mapping(new = if_else(i == 0, "COUNTRY", thisLabel),
-                           target = tibble(label = paste0("ADM", i)),
-                           source = "gadm", match = "close", certainty = 3,
+  # test whether broader concepts are all present
+  broaderMissing <- get_concept(label = items[[{{thisLabel}}]], ontology = gazetteer) |>
+    filter(is.na(id))
+  if(dim(broaderMissing)[1] != 0) stop("some broader concepts are missing from the current ontology!")
+
+  # create a new mapping to the current class
+  gazetteer <- new_mapping(new = thisLabel,
+                           target = tibble(label = paste0("ADM", i-1)),
+                           source = "gadm", match = "exact", certainty = 3,
                            type = "class", ontology = gazetteer)
 
+  # assign the new concepts into the ontology
   gazetteer <- new_concept(new = items |> pull({{thisLabel}}),
-                           broader = items |> select(id, label, class),
-                           class = paste0("ADM", i),
+                           broader = items |> select(id, label = previous_label, class),
+                           class = paste0("ADM", i-1),
                            ontology =  gazetteer)
 
+  # create a new mapping to the current labesl
   gazetteer <- new_mapping(new = items |> pull({{thisLabel}}),
                            target = left_join(items |> select(label = {{thisLabel}}, has_broader = id),
-                                              get_concept(label = items |> pull({{thisLabel}}), has_broader = items$id, class = paste0("ADM", i), ontology = gazetteer),
+                                              get_concept(label = items |> pull({{thisLabel}}), has_broader = items$id, class = paste0("ADM", i-1), ontology = gazetteer),
                                               by = c("label", "has_broader")),
                            source = "gadm", match = "close", certainty = 3,
                            type = "concept", ontology = gazetteer)
 
-  if(any(!is.na(items$alt))){
-
-    allItems <- items |>
-      separate_longer_delim(cols = alt, delim = "|")
-    gazetteer <- new_mapping(new = allItems |> pull(alt),
-                             target = left_join(allItems |> select(label = {{thisLabel}}, has_broader = id),
-                                                get_concept(label = allItems |> pull({{thisLabel}}), has_broader = allItems$id, class = paste0("ADM", i), ontology = gazetteer),
-                                                by = c("label", "has_broader")),
-                             source = "alt", match = "close", certainty = 3,
-                             ontology = gazetteer)
-  }
-
 }
-
 
 # 4. write output ----
 #
-write_rds(x = temp_geoseries, file = path_geoschemeToGADM)
+write_rds(x = temp_geo, file = paste0(.get_path("onto", "_data"), "geoscheme_to_gadm.rds"))
 write_rds(x = gazetteer, file = path_gaz)
 gaz_updated <- TRUE
-
 
 # beep(sound = 10)
 message("\n     ... done")
